f827c05e841621cdc019d6d6e0a41d4b504323d3
lilycandoit <hueduong288@gmail.com>
2024-06-29 09:56:14 +0700

    update cartQuantity based on selector

--- script/amazon.js
+++ script/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
             </div>
   
             <div class="product-quantity-container">
-              <select>
+              <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -69,20 +69,25 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       }
     });
 
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+
+    const quantitySelected = Number(quantitySelector.value);
+
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantitySelected;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: quantitySelected,
       });
     }
 
     let cartQuantity = 0;
     cart.forEach((cartItem) => {
       cartQuantity += cartItem.quantity;
-    })
+    });
     document.querySelector('.cart-quantity').innerHTML = cartQuantity;
-
   });
 });
