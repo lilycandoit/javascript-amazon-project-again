86b55e8daf92955671a67dd3c50558ffc993ed62
lily <hueduong288@gmail.com>
2024-07-04 10:09:53 +0700

    exclude weekends in deliveryDate - using while loop

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -30,10 +30,26 @@ export function getDeliveryOption(deliveryOptionId){
 
 }
 
+const isWeekend = function isWeekend(date){
+  const dayOfWeek = date.format('dddd');
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
+}
+
 export function getDeliveryDate(deliveryOption){
-  const today = dayjs();
-  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+
+  let remainingDays = deliveryOption.deliveryDays;
+
+  let deliveryDate = dayjs();// means today
+
+  while (remainingDays >0){
+    deliveryDate = deliveryDate.add(1, 'day');
+
+    if(!isWeekend(deliveryDate)){
+      remainingDays --;
+    }
+  }
   const dateString = deliveryDate.format('dddd, MMMM D');
 
   return dateString;
 }
+
