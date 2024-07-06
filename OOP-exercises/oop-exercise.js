6762c61e3af6f06a6c19a9be3227f1e797fbfbb4
lily <hueduong288@gmail.com>
2024-07-06 11:36:00 +0700

    done 17d- oop exercise

--- OOP-exercises/oop-exercise.js
+++ OOP-exercises/oop-exercise.js
@@ -7,23 +7,40 @@ class Car {
     this.model = carDetails.model;
   }
 
-  displayInfo(){
-    return `${this.brand} ${this.model}, Speed: ${this.speed} km/h`;
-  }
-
   speed = 0;
+  isTrunkopen = false;
+
+  
+  displayInfo(){
+    const trunkStatus = this.isTrunkopen ? 'open' : 'closed';
+
+    return `${this.brand} ${this.model}, Speed: ${this.speed} km/h, ${trunkStatus}`;
+  }
+  
   go(){
-    this.speed += 5;
+    if(!this.isTrunkopen){
+      this.speed += 5;
+    }
     if (this.speed > 200){
       this.speed = 200;
     }
   }
-
+  
   brake(){
     this.speed -= 5;
     if (this.speed < 0){
       this.speed = 0;
     }
+  }
+
+  openTrunk(){
+    if(this.speed === 0){
+      this.isTrunkopen = true;
+    }
+  }
+
+  closeTrunk(){
+    this.isTrunkopen = false;
   }
 }
 
@@ -40,4 +57,6 @@ car1.go();
 car1.go();
 car1.brake();
 car1.brake();
+car1.go();
 console.log(car1.displayInfo());
+// result: Toyota Corolla, Speed: 25 km/h, closed
